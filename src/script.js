a8522420aee532f25083a47147bae842b29ff071
dantes <112493972+dorukaysor@users.noreply.github.com>
2025-05-18 21:31:47 +0530

    Update script.js

--- src/script.js
+++ src/script.js
@@ -2,19 +2,6 @@
 var scene = document.getElementById('scene');
 var parallax = new Parallax(scene);
 
-// Decode 'q' parameter from URL
-function getDecodedURL() {
-  const params = new URLSearchParams(window.location.search);
-  const encoded = params.get("re");
-  if (!encoded) return null;
-  try {
-    return atob(encoded);
-  } catch (e) {
-    console.error("Invalid Base64", e);
-    return null;
-  }
-}
-
 document.getElementById("i-dare-btn").addEventListener("click", () => {
-  window.history.back();
+    window.history.back();
 });
